cf611b272026149b56358f7b0b12b730ff45ca6d
Tauhid Patel <tauhidpatel18@gmail.com>
2023-11-26 11:08:52 +0530

    feat: reset screen UI

--- src/components/Reset.tsx
+++ src/components/Reset.tsx
@@ -1,3 +1,5 @@
+import confettiImage from '../assets/confetti.png';
+
 type Props = {
     totalQuestions: number;
     correctQuestions: number;
@@ -5,11 +7,23 @@ type Props = {
 };
 
 function Reset(props: Props) {
+
+
     return (
         <div className="reset-container">
+            <img src={confettiImage} alt="" />
             <p>Congratulations on Finishing the trivia, you did a great job !</p>
-            <h1>You scored: {(props.correctQuestions / props.totalQuestions)*100}%</h1>
-            <button onClick={props.onPress}>Try again!</button>
+            <h1>You scored: {((props.correctQuestions / props.totalQuestions) * 100).toFixed(0)}%</h1>
+            <div className="btn-container my-2">
+                <button type="button" className="btn btn-success" style={{marginRight: '1em'}}>
+                Right <span style={{marginLeft: '1em'}} className="badge text-bg-light">{props.correctQuestions}</span>
+                </button>
+                <button type="button" className="btn btn-danger">
+                Wrong <span style={{marginLeft: '1em'}} className="badge text-bg-light">{props.totalQuestions - props.correctQuestions}</span>
+                </button>
+            </div>
+            
+            <button className="btn-tryagain" onClick={props.onPress}>Try again!</button>
         </div>
     )
 }
